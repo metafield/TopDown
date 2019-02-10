d953acdfeb621d6657722afec92cfa1ae4aac2b4
metafield <anthony.sylvester99@gmail.com>
2019-02-10 13:47:10 -0800

    creates a new FPSDisplay component that will oberser the FPS in FPScounter

---
+++ src/components/FPSDisplay.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import FPScounter from '../FPScounter';
+
+import { observer } from 'mobx-react';
+
+@observer
+class FPSDisplay extends React.Component {
+    public render() {
+        return <React.Fragment>
+        <p>{FPScounter.fps}</p>
+    </React.Fragment>
+    }
+}
+
+export default FPSDisplay;
