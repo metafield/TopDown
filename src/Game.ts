f48bb687e6bab769f08cb62fd103173e1a2bfbc6
metafield <anthony.sylvester99@gmail.com>
2019-02-11 22:20:37 -0800

    Tests out delta time using some debug methods and values

--- src/Game.ts
+++ src/Game.ts
@@ -3,12 +3,17 @@ import { when } from 'mobx';
 
 import FPScounter from './FPScounter'
 
+import sheetPNG from './assets/0x72_DungeonTilesetII_v1.2.png'
+
 class Game  {
     private ctx: CanvasRenderingContext2D;
-    private startX: number = 10;
-    private startY: number = 10;
+    private sheet = new Image()
 
-    constructor(public canvas:any){}
+    private demonAnimTracker = 0;
+
+    constructor(public canvas:any){
+        this.sheet.src = sheetPNG;
+    }
 
     public async start() {
         await when(() => this.canvas !== null)
@@ -30,26 +35,46 @@ class Game  {
     
     private async tick() {
         FPScounter.startCounter()
-        this.render()
+        this.render(FPScounter.delta)
         await this.sleep(3)
         FPScounter.StopAndPost()
         requestAnimationFrame(() => this.tick())
     }
 
-    private render() {
+    private render(dt: number) {
+        this.drawDemon(dt)
+    }
+
+    private drawDemon(dt: number) {
+        const bigDemonRunAnim = {
+            "x": 144,
+            "y": 364,
+            "w": 32,
+            "h": 36,
+            "animLen": 4,
+            "isAnim": true
+        }
+
+        // add dt to tracked time
+        this.demonAnimTracker += dt;
+        const msPerAnim = 30;
+        const rawFrame = Math.floor(this.demonAnimTracker / msPerAnim)
+
+        let curFrame = rawFrame;
+
+        if (rawFrame >= bigDemonRunAnim.animLen) {
+            curFrame = 1;
+            this.demonAnimTracker = 0;
+        }
+
+        const { x, y , w, h } = bigDemonRunAnim;
+        console.log(curFrame);
+        
+
         const ctx = this.ctx
-
-        ctx.clearRect(0,0,800,600)
-
-
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(this.startX, this.startY, 50, 50);
-
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(this.startX + 20, this.startY + 20, 50, 50);
-
-        this.startX++
-        this.startY++
+        const sx = (curFrame * 32) + x
+        ctx.clearRect(0,0,600,800)
+        ctx.drawImage(this.sheet, sx, y, w, h, 0, 0, w, h)
     }
 }
 
