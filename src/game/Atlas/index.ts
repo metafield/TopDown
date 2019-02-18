030c8fcc8867aa4cb4f8ade6ed18913da4307617
metafield <anthony.sylvester99@gmail.com>
2019-02-18 14:22:17 -0800

    adds three floors to the atlas

--- src/game/Atlas/index.ts
+++ src/game/Atlas/index.ts
@@ -17,4 +17,25 @@ const demon = {
     },
 }
 
-export { demon }
+const floors = [
+    {
+        x: 16,
+        y: 64,
+        w: 16,
+        h: 16,
+    },
+    {
+        x: 32,
+        y: 64,
+        w: 16,
+        h: 16,
+    },
+    {
+        x: 48,
+        y: 64,
+        w: 16,
+        h: 16,
+    },
+]
+
+export { demon, floors }
