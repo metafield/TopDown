30632dfc9531f153a06303441cac78fe614cea6d
metafield <anthony.sylvester99@gmail.com>
2019-02-17 20:36:35 -0800

    prettier

--- src/components/FPSDisplay.tsx
+++ src/components/FPSDisplay.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import FPScounter from '../game/FPScounter';
+import * as React from 'react'
+import FPScounter from '../game/FPScounter'
 
-import { observer } from 'mobx-react';
+import { observer } from 'mobx-react'
 
 @observer
 class FPSDisplay extends React.Component {
     public render() {
-        return <React.Fragment>
-        <p>{FPScounter.fps}fps</p>
-    </React.Fragment>
+        return (
+            <React.Fragment>
+                <p>{FPScounter.fps}fps</p>
+            </React.Fragment>
+        )
     }
 }
 
-export default FPSDisplay;
+export default FPSDisplay
